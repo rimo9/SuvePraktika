c78e8d7cf3aaaa182f5c905a2f469009df048a33
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2016-06-20 12:50:25 +0300

    removed test

--- pages/test.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <form>
-    <form method="get">
-      <label>Name: </label>
-      <input type="text" id="name" name="name">
-
-      <input type="submit" id="submit" value="done">
-  </form>
-</html>
